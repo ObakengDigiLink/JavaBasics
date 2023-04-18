86c31e7fc5f2eda221cb91fc84c6398d2b2cedd0
Obakeng Pitse <obakeng@harambee.co.za>
2023-04-18 09:42:04 +0200

    Variables and if else

---
+++ Variables.java
@@ -0,0 +1,17 @@
+public class Variables {
+    public static void main(String[] args) {
+        String productName = "Shoes";
+        int quantity = 5 ;
+        Double price = 699.99;
+        boolean isAvailable = true;
+
+        System.out.println("Product name: "+productName);
+        System.out.println("Quantity: "+quantity);
+        System.out.println("Price: "+price);
+        if(isAvailable) {
+            System.out.println("Product is Available");
+        }else {
+            System.out.println("Product is Not Available");
+        }
+    }
+}
